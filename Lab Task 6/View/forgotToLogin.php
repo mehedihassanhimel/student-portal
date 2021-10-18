5e09a4f9d7f03bca4c22109e32237c077cfec400
mehedihassanhimel <mehedihassan5676@gmail.com>
2021-10-18 22:59:09 +0600

    Old lab task folders removed

--- Lab Task 6/View/forgotToLogin.php
+++
@@ -1,5 +0,0 @@
-<?php  
-   echo "A link has been to sent to reset your password";
-   echo "<p>Go Back To <a href='loginV.php'>Login</a> Page</p>";
-
-?> 
--- Lab Task 6/View/regToLogin.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-   echo "Registration Completed  Successfully";
-   echo "<p>Go Back To <a href='loginV.php'>Login</a> Page</p>";
-
-?> 
